00d77d4855851173cd6d21a0d72f3fe8ffbe8827
baselkorj <baselanwarkorj@gmail.com>
2021-03-31 21:00:52 +0400

    test commit, created gui class

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# LED Matrix project using raspberryPi
+# Control an LED Matrix through Raspberry Pi & Tkinter
+
+# Importing Libraries
+import tkinter as tk
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+
+if __name__ == "__main__":
+    app = App()
+    app.title("LED Matrix Controller")
+    app.geometry("500x500")
+    app.mainloop()
